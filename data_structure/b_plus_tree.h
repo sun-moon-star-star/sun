620be82d6793bfd3784e27b09aad9402aa3154d9
sun-moon-star-star <75414051+sun-moon-star-star@users.noreply.github.com>
2020-12-14 10:21:04 +0800

    feat(data_structure/b_plus_tree): insert

--- data_structure/b_plus_tree.h
+++ data_structure/b_plus_tree.h
@@ -26,9 +26,9 @@ struct b_plus_tree_leaf_node {
     unsigned int head : 16;
     unsigned int free : 16;
     b_plus_tree_node_index idx[m];
+    b_plus_tree_leaf_node* nxt;
     KeyType key[m];
     ValueType ch[m];
-    b_plus_tree_leaf_node* pre, nxt;
 }; // struct b_plus_tree_leaf_node
 
 template <typename KeyType, typename ValueType, unsigned m>
@@ -37,17 +37,17 @@ struct b_plus_tree_node {
     unsigned int head : 16;
     unsigned int free : 16;
     b_plus_tree_node_index idx[m];
-    KeyType key[m]; // parent store max_key
     union {
         b_plus_tree_node *no_leaf;
-        b_plus_tree_leaf_node<KeyType,ValueType,m> *leaf;
+        b_plus_tree_leaf_node<KeyType, ValueType, m> *leaf;
     } ch[m];
+    KeyType key[m]; // parent store max_key
 }; // struct b_plus_tree_node
 
 // 2 < m < 65535
 template <typename KeyType, typename ValueType, unsigned m,
     typename KeyCompareFunc = less<KeyType>,
-    typename = typename std::enable_if<(m > 2) && (m < 65535)>::type >
+    typename = typename std::enable_if<(m > 2) && (m < 65535)>::type>
 class b_plus_tree {
 private:
     using node = b_plus_tree_node<KeyType, ValueType, m>;
@@ -59,17 +59,45 @@ public:
         _fill_min(std::min(std::max(2, fill_min), m), _size(0), _depth(0), _root(nullptr) {
     }
 
+    // todo
+    b_plus_tree(const b_plus_tree& other) {
+
+    }
+
+    // todo
+    virtual b_plus_tree& operator=(const b_plus_tree& other) {
+        return *this;
+    }
+
+    // todo
+    virtual ~b_plus_tree() {
+
+    }
+
 private:
     bool compare_node(const KeyType& x, const KeyType& y) const {
         return _key_compare_func(x, y);
     }
 
-    leaf_node* create_new_leaf_node() {
-        leaf_node* new_node = reinterpret_cast<leaf_node*>(malloc(sizeof(leaf_node)));
+    template <typename NodeType, bool is_leaf = std::is_same<NodeType, leaf_node>::value,
+        typename = std::enable_if <std::is_same<NodeType, node>::value
+                                    || std::is_same<NodeType, leaf_node>::value>::type>
+    void node_ch_setter(NodeType* node, unsigned int idx, void* ptr) {
+        if (is_leaf) {
+            node->ch[idx] = *reinterpret_cast<ValueType*>(ptr);
+        } else {
+            node->ch[idx].leaf = reinterpret_cast<leaf_node*>(ptr);
+        }
+    }
+
+    template <typename NodeType>
+    typename std::enable_if <std::is_same<NodeType, node>::value
+                            || std::is_same<NodeType, leaf_node>::value, NodeType*>::type
+    create_new_node_common() {
+        NodeType* new_node = reinterpret_cast<NodeType*>(malloc(sizeof(NodeType)));
 
         new_node->head = END;
         new_node->free = new_node->num = 0;
-        new_node->pre = new_node->nxt = nullptr;
 
         for (int i = 0; i < m - 1; ++i) {
             new_node->idx[i].nxt = i + 1;
@@ -79,34 +107,48 @@ private:
         return new_node;
     }
 
-    node* create_new_no_leaf_node() {
-        node* new_node = reinterpret_cast<node*>(malloc(sizeof(node)));
-
-        new_node->head = END;
-        new_node->free = new_node->num = 0;
-
-        for (int i = 0; i < m - 1; ++i) {
-            new_node->idx[i].nxt = i + 1;
-        }
-        new_node->idx[m - 1].nxt = END;
-
+    leaf_node* create_new_leaf_node() {
+        leaf_node* new_node = create_new_node_common<leaf_node>();
+        new_node->nxt = nullptr;
         return new_node;
+    }
+
+    node* create_new_no_leaf_node() {
+        return create_new_node_common<node>();
+    }
+
+    template <typename NodeType, bool is_leaf = std::is_same<NodeType, leaf_node>::value>
+    typename std::enable_if <std::is_same<NodeType, node>::value
+                            || std::is_same<NodeType, leaf_node>::value, NodeType*>::type
+    create_new_node() {
+        if (is_leaf) {
+            return reinterpret_cast<NodeType*>(create_new_leaf_node());
+        }
+        return reinterpret_cast<NodeType*>(create_new_no_leaf_node());
+    }
+
+    template <typename NodeType>
+    std::enable_if <std::is_same<NodeType, node>::value
+                    || std::is_same<NodeType, leaf_node>::value, unsigned int>::type
+    alloc_data_in_node_common(NodeType* node_item, const KeyType& key) {
+        ++node_item->num;
+
+        unsigned int idx = node_item->free;
+        node_item->free = node_item->idx[idx].nxt;
+
+        node_item->key[idx] = key;
+        return idx;
     }
 
     /**
      * @before: leaf has space(leaf->num < m)
      * @after: adjust head idx in leaf node
      */
-    unsigned int alloc_data_in_leaf_node(leaf_node* leaf, const KeyType& key, const ValueType& value) {
+    unsigned int alloc_data_in_leaf_node(leaf_node* leaf, const KeyType& key, void* ptr) {
+        unsigned int idx = alloc_data_in_node_common<leaf_node>(leaf, key);
+
+        leaf->ch[idx] = *reinterpret_cast<ValueType*>(ptr);
         ++_size;
-
-        ++leaf->num;
-
-        unsigned int idx = leaf->free;
-        leaf->free = leaf->idx[idx].nxt;
-
-        leaf->key[idx] = key;
-        leaf->ch[idx] = value;
 
         return idx;
     }
@@ -116,56 +158,33 @@ private:
      * @after: adjust head idx in no_leaf node, set ch[idx]
      * @return idx(new node index in no_leaf node)
      */
-    unsigned int alloc_data_in_no_leaf_node(node* no_leaf, const KeyType& key) {
-        ++no_leaf->num;
+    unsigned int alloc_data_in_no_leaf_node(node* no_leaf, const KeyType& key, void* ptr) {
+        unsigned int idx = alloc_data_in_node_common<node>(no_leaf, key);
 
-        unsigned int idx = no_leaf->free;
-        no_leaf->free = no_leaf->idx[idx].nxt;
-
-        no_leaf->key[idx] = key;
-
+        no_leaf->ch[idx].leaf = reinterpret_cast<leaf_node*>(ptr);
+        
         return idx;
     }
 
-    /**
-     * @before: leaf has space(leaf->num < m)
-     */ 
-    unsigned insert_into_leaf_node(leaf_node* leaf, const KeyType& key, const ValueType& value) {    
-        unsigned int last = END;
-        unsigned int at = leaf->head;
-        for (; at != END; last = at, at = leaf->idx[at].nxt) {
-            if (compare_node(key, leaf->key[at])) {
-                if (compare_node(leaf->key[at], key)) {
-                    // equal, modify already exist node
-                    leaf->ch[at] = value;
-                    return;
-                }
-                break;
-            }
+    template <typename NodeType, bool is_leaf = std::is_same<NodeType, leaf_node>::value>
+    typename std::enable_if <std::is_same<NodeType, node>::value
+                            || std::is_same<NodeType, leaf_node>::value, unsigned int>::type
+    alloc_data_in_node(NodeType* node, const KeyType& key, void* ptr) {
+        if (is_leaf) {
+            return alloc_data_in_leaf_node(node, key, ptr);
         }
-
-        // alloc new
-        unsigned int idx = alloc_data_in_leaf_node(leaf, key, value);
-        if (unlikely(last == END)) {
-            leaf->idx[idx].nxt = leaf->head;
-            leaf->head = idx;
-        } else {
-            leaf->idx[idx].nxt = at;
-            leaf->idx[last].nxt = idx;
-        }
+        return alloc_data_in_no_leaf_node(node, key, ptr);
     }
 
-    /**
-     * @before: no_leaf has space(no_leaf->num < m)
-     */ 
-    unsigned insert_into_no_leaf_node(node* no_leaf, const KeyType& key) {    
+    template <typename NodeType>
+    unsigned int insert_into_node(NodeType* node, const KeyType& key, void* ptr) {
         unsigned int last = END;
-        unsigned int at = leaf->head;
-        for (; at != END; last = at, at = leaf->idx[at].nxt) {
-            if (compare_node(key, leaf->key[at])) {
-                if (compare_node(leaf->key[at], key)) {
+        unsigned int at = node->head;
+        for (; at != END; last = at, at = node->idx[at].nxt) {
+            if (compare_node(key, node->key[at])) {
+                if (compare_node(node->key[at], key)) {
                     // equal, modify already exist node
-                    leaf->ch[at] = value;
+                    node->key[at] = key;
                     return;
                 }
                 break;
@@ -173,7 +192,7 @@ private:
         }
 
         // alloc new
-        unsigned int idx = alloc_data_in_leaf_node(leaf, key, value);
+        unsigned int idx = alloc_data_in_node<NodeType>(node, key, value);
         if (unlikely(last == END)) {
             leaf->idx[idx].nxt = leaf->head;
             leaf->head = idx;
@@ -181,15 +200,30 @@ private:
             leaf->idx[idx].nxt = at;
             leaf->idx[last].nxt = idx;
         }
+
     }
 
     void insert_when_less_then_m(const KeyType& key, const ValueType& value) {
         if (_size == 0) {
             _depth = 1;
-            _root.leaf = _root.leaf || create_new_leaf_node();
+            _root.leaf = _root.leaf || create_new_node<leaf_node>();
         }
 
-        return insert_into_leaf_node(_root.leaf, key, value);
+        return insert_into_node<leaf_node>(_root.leaf, key, value);
+    }
+
+    /**
+     * get at m / 2 - 1 node index
+     */
+    template <typename NodeType>
+    typename std::enable_if <std::is_same<NodeType, node>::value
+                            || std::is_same<NodeType, leaf_node>::value, unsigned int>::type
+    get_node_half_last(NodeType* item) {
+        unsigned int idx = item->head;
+        for (int i = 0; i < m / 2 - 1; ++i) {
+            idx = item->idx[idx].nxt;
+        }
+        return idx;
     }
 
     /**
@@ -197,14 +231,11 @@ private:
      * @before: no_leaf->num == m, fa has space and fa.ch[idx] is point to no_leaf
      */
     node* split_no_leaf_node_unit(node* no_leaf, node* fa, unsigned int idx) {
-        node* new_node = create_new_no_leaf_node();
-        unsigned int idx = no_leaf->head;
-        unsigned int last = END;
-        for (int i = 0; i < m/2; ++i) {
-            last = idx;
-            idx = no_leaf->idx[idx].nxt;
-        }
+        node* new_node = create_new_node<node>();
 
+        unsigned int last = get_node_half_last<node>(no_leaf);
+
+        unsigned int idx = no_leaf->idx[last].nxt;
         no_leaf->free = idx;
         no_leaf->idx[last].nxt = END;
         no_leaf->num = m / 2;
@@ -235,14 +266,10 @@ private:
      * @before: leaf->num == m, fa has space(fa->num < m) and fa.ch[idx] is point to leaf
      */
     leaf_node* split_leaf_node_unit(leaf_node* leaf, node* fa, unsigned int idx) {
-        leaf_node* new_node = create_new_leaf_node();
-        unsigned int idx = leaf->head;
-        unsigned int last = END;
-        for (int i = 0; i < m/2; ++i) {
-            last = idx;
-            idx = leaf->idx[idx].nxt;
-        }
+        leaf_node* new_node = create_new_node<leaf_node>();
 
+        unsigned int last = get_node_half_last<leaf_node>(leaf);
+        
         leaf->free = idx;
         leaf->idx[last].nxt = END;
         leaf->num = m / 2;
@@ -266,6 +293,17 @@ private:
         fa->idx[idx].nxt = new_idx_in_fa;        
 
         return new_node;
+    }
+
+    leaf_node* split_when_root_is_leaf_node() {
+        node* root = create_new_no_leaf_node();
+        
+        unsigned int new_idx = root->free;
+        root->free = root->idx[new_idx].nxt;
+        root->idx[new_idx].nxt = root->head;
+        root->head = new_idx;
+        root->key[] =
+
     }
 
 public:
@@ -303,12 +341,19 @@ public:
             return insert_into_leaf_node(leaf, key, value);
         }
 
+        if (_depth = 1) {
+            // leaf is leaf
 
+        }
 
-        
+    }
 
-
-
+    /**
+     * @brief 根据key删除数据
+     * @param[in] key 插入节点的key
+     * @param[in] value 删除数据的value
+     */
+    void delete(const KeyType& key, ValueType* value) {
 
     }
 
