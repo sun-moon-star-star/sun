2df08bbb977302c3c6e427e7b3a3c5f08ce4c0c4
sun-moon-star-star <75414051+sun-moon-star-star@users.noreply.github.com>
2020-12-26 17:14:18 +0800

    feat(util/hash): init

--- sun/util/hash.h
+++ sun/util/hash.h
@@ -7,21 +7,204 @@
 #define SUN_UTIL_HASH_H_
 
 #include <cstdint>
+#include <string>
+#include <type_traits>
 
-namespace sun {
+namespace sun::util {
 
-namespace util {
+namespace __1 {
 
-using hash_code_t = std::uint64_t;
+template <typename T, uint8_t bytes = sizeof(T)>
+struct get_bytes {
+  typedef uint64_t type;
+};
 
 template <typename T>
-hash_code_t hash_code() {
-  hash_code_t code = 0;
+struct get_bytes<T, 1> {
+  typedef uint8_t type;
+};
+
+template <typename T>
+struct get_bytes<T, 2> {
+  typedef uint16_t type;
+};
+
+template <typename T>
+struct get_bytes<T, 4> {
+  typedef uint32_t type;
+};
+
+#ifndef _LIBCPP_HAS_NO_INT128
+template <typename T>
+struct get_bytes<T, 16> {
+  typedef __uint128_t type;
+};
+#endif
+
+template <typename T, uint8_t bytes = sizeof(T)>
+struct get_hash_code_len {
+  typedef uint64_t type;
+};
+
+#ifndef _LIBCPP_HAS_NO_INT128
+template <typename T>
+struct get_hash_code_len<T, 16> {
+  typedef __uint128_t type;
+};
+#endif
+
+template <class T, uint8_t bytes, std::size_t N>
+struct get_hash_code_len<T[N], bytes> {
+  typedef get_hash_code_len<T> type;
+};
+
+union ptr_getter {
+  const void* ptr;
+  uint64_t code;
+};  // union ptr_getter
+
+const uint64_t HASH_FACTOR = 11131;
+
+}  // namespace __1
+
+/**
+ * @brief hash_code 默认取地址
+ */
+template <typename T, typename V = void>
+struct hash_code_helper {
+  hash_code_helper(T&& t) {
+    __1::ptr_getter getter;
+    getter.ptr = &t;
+    code = getter.code;
+  }
+  uint64_t get() const { return code; }
+
+ private:
+  uint64_t code;
+};
+
+template <typename T>
+struct hash_code_helper<
+    T, std::enable_if_t<std::is_integral_v<std::remove_reference_t<T>> ||
+                        std::is_floating_point_v<std::remove_reference_t<T>>>> {
+  typedef typename __1::get_hash_code_len<T>::type code_type;
+
+  hash_code_helper(T&& t) {
+    __1::ptr_getter getter;
+    getter.ptr = &t;
+    code = 1llu * (*reinterpret_cast<const typename __1::get_bytes<T>::type*>(
+                      getter.ptr));
+  }
+
+  code_type get() const { return code; }
+
+ private:
+  code_type code;
+};
+
+template <typename T, std::size_t N>
+struct hash_code_helper<T (&)[N]> {
+  typedef typename __1::get_hash_code_len<T>::type code_type;
+
+  hash_code_helper(T (&t)[N]) {
+    code = 0;
+    for (std::size_t i = 0; i < N; ++i) {
+      code = code * __1::HASH_FACTOR + hash_code_helper<T&>(t[i]).get();
+    }
+  }
+
+  code_type get() const { return code; }
+
+ private:
+  code_type code;
+};
+
+template <typename T, std::size_t N>
+struct hash_code_helper<T(&&)[N]> {
+  typedef typename __1::get_hash_code_len<T>::type code_type;
+
+  hash_code_helper(T(&&t)[N]) {
+    code = 0;
+    for (std::size_t i = 0; i < N; ++i) {
+      code = code * __1::HASH_FACTOR +
+             hash_code_helper<T&&>(std::move(t[i])).get();
+    }
+  }
+
+  code_type get() const { return code; }
+
+ private:
+  code_type code;
+};
+
+template <std::size_t N>
+struct hash_code_helper<char (&)[N]> {
+  typedef typename __1::get_hash_code_len<char>::type code_type;
+
+  hash_code_helper(char (&t)[N]) {
+    code = 0;
+    for (std::size_t i = 0; t[i] != '\0'; ++i) {
+      code = code * __1::HASH_FACTOR + hash_code_helper<char&>(t[i]).get();
+    }
+  }
+
+  code_type get() const { return code; }
+
+ private:
+  code_type code;
+};
+
+template <std::size_t N>
+struct hash_code_helper<char(&&)[N]> {
+  typedef typename __1::get_hash_code_len<char>::type code_type;
+
+  hash_code_helper(char(&&t)[N]) {
+    code = 0;
+    for (std::size_t i = 0; t[i] != '\0'; ++i) {
+      code = code * __1::HASH_FACTOR +
+             hash_code_helper<char&&>(std::move(t[i])).get();
+    }
+  }
+
+  code_type get() const { return code; }
+
+ private:
+  code_type code;
+};
+
+template <typename T>
+struct hash_code_helper<
+    T, std::enable_if_t<std::is_same_v<
+           std::string, std::remove_cv_t<std::remove_reference_t<T>>>>> {
+  typedef typename __1::get_hash_code_len<char>::type code_type;
+
+  hash_code_helper(T&& t) {
+    code = 0;
+    for (std::size_t i = 0; t[i] != '\0'; ++i) {
+      code =
+          code * __1::HASH_FACTOR + hash_code_helper<const char&>(t[i]).get();
+    }
+  }
+
+  code_type get() const { return code; }
+
+ private:
+  code_type code;
+};
+
+template <typename T>
+decltype(auto) hash_code(T&& t) {
+  return hash_code_helper<T&&>(std::forward<T>(t)).get();
+}
+
+uint64_t hash_code(const char* const ptr) {
+  uint64_t code = 0;
+  for (std::size_t i = 0; ptr[i] != '\0'; ++i) {
+    code = code * __1::HASH_FACTOR + hash_code(ptr[i]);
+  }
   return code;
 }
 
-}  // namespace util
-
-}  // namespace sun
+}  // namespace sun::util
 
 #endif  // SUN_UTIL_HASH_H_
---
+++ sun/util/hash_test.cc
@@ -0,0 +1,120 @@
+/**
+ * @file hash_test.cc
+ * @author sun-moon-star-star
+ */
+
+#include "sun/util/hash.h"
+
+#include <string>
+
+#include <gtest/gtest.h>
+
+class HashTest : public testing::Test {};
+
+using sun::util::hash_code;
+
+TEST_F(HashTest, test_int) {
+  ASSERT_EQ(hash_code(990303), 990303llu);
+  ASSERT_EQ(hash_code(990303u), 990303llu);
+  ASSERT_EQ(hash_code(990303ll), 990303llu);
+  ASSERT_EQ(hash_code(990303llu), 990303llu);
+
+#ifndef _LIBCPP_HAS_NO_INT128
+  uint64_t i64 = __LONG_LONG_MAX__;
+  __uint128_t i128 = i64;
+
+  ASSERT_EQ(hash_code(i64), hash_code(i128));
+  ASSERT_EQ(typeid(hash_code(i64)), typeid(uint64_t));
+
+  i128 *= 1000;
+  i64 *= 1000;
+
+  ASSERT_NE(hash_code(i64), hash_code(i128));
+#endif
+}
+
+TEST_F(HashTest, test_float) {
+  ASSERT_EQ(hash_code(99.0303f), hash_code(99.0303f));
+  ASSERT_NE(hash_code(99.0303f), hash_code(99.0303));
+  ASSERT_EQ(hash_code(99.0303), hash_code(99.0303));
+
+  float f = 99.0303;
+  double d = 99.0303;
+
+  ASSERT_NE(hash_code(f), hash_code(99.0303));
+  ASSERT_NE(hash_code(d), hash_code(99.0303f));
+}
+
+TEST_F(HashTest, test_char_pointer) {
+  char str[] = "hello";
+  std::string str2 = "hello";
+  const std::string str3 = "hello";
+  std::string& str4 = str2;
+  const std::string& str5 = "hello";
+
+  ASSERT_EQ(hash_code("hello"), hash_code("hello"));
+  ASSERT_EQ(hash_code("hello"), hash_code(std::string("hello")));
+  ASSERT_EQ(hash_code("hello"), hash_code(str));
+  ASSERT_EQ(hash_code("hello"), hash_code(str2));
+  ASSERT_EQ(hash_code("hello"), hash_code(str3));
+  ASSERT_EQ(hash_code("hello"), hash_code(str4));
+  ASSERT_EQ(hash_code("hello"), hash_code(str5));
+  ASSERT_NE(hash_code("hello"), hash_code("world"));
+}
+
+struct A {
+  A(int i, double d, std::string str) : i(i), d(d), str(str) {}
+  uint64_t hash_code() const {
+    uint64_t code = ::hash_code(i);
+    code += ::hash_code(d);
+    code += ::hash_code(str);
+    return code;
+  }
+
+  int i;
+  double d;
+  std::string str;
+};
+
+template <typename T>
+struct sun::util::hash_code_helper<
+    T, std::enable_if_t<
+           std::is_same_v<A, std::remove_cv_t<std::remove_reference_t<T>>>>> {
+  hash_code_helper(T&& t) : code(t.hash_code()) {}
+
+  uint64_t get() const { return code; }
+
+ private:
+  uint64_t code;
+};
+
+TEST_F(HashTest, test_struct) {
+  A a(1, 9903.03, "zhao");
+  A& b = a;
+  const A c = a;
+  const A& d = a;
+
+  uint64_t hash_code_v = hash_code(1);
+  hash_code_v += hash_code(9903.03);
+  hash_code_v += hash_code("zhao");
+
+  ASSERT_EQ(hash_code(a), hash_code_v);
+  ASSERT_EQ(hash_code(a), hash_code(b));
+  ASSERT_EQ(hash_code(a), hash_code(c));
+  ASSERT_EQ(hash_code(a), hash_code(d));
+}
+
+TEST_F(HashTest, test_struct_array) {
+  A a(1, 9903.03, "zhao");
+  A b(2, 0208.21, "yu");
+
+  A array[] = {a, b};
+  A array2[2] = {A{1, 9903.03, "zhao"}, A{2, 0208.21, "yu"}};
+
+  ASSERT_EQ(hash_code(array), hash_code(array2));
+}
+
+int main(int argc, char** argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
