9e86d987a816632780d7ec578d81a704f16af579
sun-moon-star-star <75414051+sun-moon-star-star@users.noreply.github.com>
2021-01-02 01:35:05 +0800

    feat(net/connection): init

---
+++ sun/net/connection.h
@@ -0,0 +1,11 @@
+/**
+ * @file connection.h
+ * @author sun-moon-star-star
+ */
+
+#ifndef SUN_NET_CONNECTION_H_
+#define SUN_NET_CONNECTION_H_
+
+namespace sun::net {}  // namespace sun::net
+
+#endif  // SUN_NET_CONNECTION_H_
