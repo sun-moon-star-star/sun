43b231574d344ba2b33633884040b3f0751d8433
sun-moon-star-star <75414051+sun-moon-star-star@users.noreply.github.com>
2021-01-07 04:46:59 +0800

    feat(util/singleton): init

---
+++ sun/util/singleton.h
@@ -0,0 +1,46 @@
+/**
+ * @file singleton.h
+ * @author sun-moon-star-star
+ */
+
+#ifndef SUN_UTIL_SINGLETON_H_
+#define SUN_UTIL_SINGLETON_H_
+
+#include <atomic>
+#include <mutex>
+
+namespace sun::util {
+
+template <typename T>
+class singleton final {
+ public:
+  static T* GetInstance() {
+    T* tmp = _instance.load(std::memory_order::memory_order_acquire);
+    if (tmp == nullptr) {
+      std::lock_guard<std::mutex> lock(_mutex);
+      tmp = instance_.load(std::memory_order::memory_order_relaxed);
+      if (_instance == nullptr) {
+        tmp = new T;
+        _instance.store(tmp, std::memory_order::memory_order_release);
+      }
+    }
+    return _instance;
+  }
+
+ protected:
+  static std::mutex _mutex;
+  static std::atomic<T*> _instance;
+  Singleton() {}
+  Singleton(const Singleton&);
+  Singleton& operator=(const Singleton&);
+};
+
+template <typename T>
+std::atomic<T*> Singleton<T>::_instance = nullptr;
+
+template <typename T>
+std::mutex Singleton<T>::_mutex;
+
+}  // namespace sun::util
+
+#endif  // SUN_UTIL_SINGLETON_H_
