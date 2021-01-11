ca7bba88c2100b5b0dbf2b6f097153d294d9c35c
sun-moon-star-star <75414051+sun-moon-star-star@users.noreply.github.com>
2021-01-11 14:51:30 +0800

    feat(sync/task_queue)

--- sun/sync/task_queue.h
+++ sun/sync/task_queue.h
@@ -12,22 +12,30 @@
 #include "sun/sync/queue.h"
 
 #include <atomic>
+#include <condition_variable>
 #include <cstdint>
 #include <functional>
+#include <mutex>
 #include <thread>
 
 namespace sun::sync {
 
 class task_queue {
  public:
-  using task = std::function<void(void*)>;
+  using call = std::function<void(void*)>;
+
+  using task = struct {
+    void* data;
+    call func;
+  };
 
   task_queue(const uint16_t core_thread_num, const uint16_t max_thread_num,
-             const uint32_t max_task_num)
+             const uint32_t core_task_num, const uint32_t max_task_num)
       : _core_thread_num(core_thread_num),
         _max_thread_num(max_thread_num),
+        _core_task_num(core_task_num),
         _max_task_num(max_task_num),
-        __task_num(0u),
+        _task_num(0u),
         _is_stop(false),
         _refuse_task(false) {}
 
@@ -40,31 +48,50 @@ class task_queue {
   }
 
  private:
+  bool run_one_task() {
+    bool get_task = false;
+
+    if (_urgent_tasks.count()) {
+      get_task = _urgent_tasks.try_pop(&t);
+    }
+
+    if (get_task == false && _priority_tasks.size()) {
+      get_task = _priority_tasks.pop(&t);
+    }
+
+    if (get_task == false && _normal_tasks.size()) {
+      get_task = _normal_tasks.try_pop(&t);
+    }
+
+    if (get_task) {
+      t.func(t.data);
+      return true;
+    }
+
+    return false;
+  }
+
   // thread
-  void run() {
-    bool last_get_task = true;
+  void core_run() {
     while (_is_stop.load(std::memory_order::memory_order_acquire)) {
       task t;
-      bool get_task = false;
-      if (_urgent_tasks.count()) {
-        get_task = _urgent_tasks.try_pop(&t);
+
+      {
+        std::unique_lock<LockType> lock(_lock);
+        _idle.wait(lock, [&] { return _task_num > 0; });
       }
 
-      if (get_task == false && _priority_tasks.size()) {
-        get_task = _priority_tasks.pop(&t);
+      run_one_task();
+    }
+  }
+
+  void extra_run() {
+    while (_is_stop.load(std::memory_order::memory_order_acquire)) {
+      if (_task_num < _core_task_num) {
+        break;
       }
 
-      if (get_task == false && _normal_tasks.size()) {
-        get_task = _urgent_tasks.try_pop(&t);
-      }
-
-      if (get_task == false) {
-        // todo (condition_varible)
-        if (last_get_task == false) {
-        }
-      }
-
-      last_get_task = get_task;
+      run_one_task();
     }
   }
 
@@ -82,11 +109,14 @@ class task_queue {
     }
 
     {
-      const std::lock_guard<LockType> lock(_lock);
+      const std::unique_lock<LockType> lock(_lock);
       if (_task_num >= _max_task_num) {
         return false;
       }
-      ++_task_num;  // must
+      if (++_task_num == 1) {
+        lock.unlock();
+        _idle.notify_one();
+      }
     }
 
     return true;
@@ -122,16 +152,19 @@ class task_queue {
  private:
   const uint16_t _core_thread_num;
   const uint16_t _max_thread_num;
+  const uint32_t _core_task_num;
   const uint32_t _max_task_num;
   uint32_t _task_num;
   std::atomic_bool _is_stop;
   std::atomic_bool _is_shundown;
   LockType _lock;
+  std::condition_variable_any _idle;
   queue<task> _urgent_tasks;
   heap<uint8_t, task> _priority_tasks;
   queue<task> _normal_tasks;
   list<std::thread> _threads;
-} __attribute__((__packed__));  // class task_queue
+}  // namespace sun::sync
+__attribute__((__packed__));  // class task_queue
 
 }  // namespace sun::sync
 
