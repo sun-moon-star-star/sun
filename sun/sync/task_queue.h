92ba7d78c37ac10bf5bfd9fb733fc0cc586ccd8f
sun-moon-star-star <75414051+sun-moon-star-star@users.noreply.github.com>
2021-01-12 13:23:54 +0800

    feat(sync/task_queue): thread

--- sun/sync/task_queue.h
+++ sun/sync/task_queue.h
@@ -37,13 +37,16 @@ class task_queue {
         _max_task_num(max_task_num),
         _task_num(0u),
         _is_stop(false),
-        _refuse_task(false) {}
+        _refuse_task(false),
+        _thread_num(0) {}
 
   ~task_queue() {
     std::thread t;
     while (_threads.size() != 0) {
       _threads.pop_back(&t);
-      t.join();
+      if (t.joinable()) {
+        t.join();
+      }
     }
   }
 
@@ -71,7 +74,7 @@ class task_queue {
     return false;
   }
 
-  // thread
+  // core thread
   void core_run() {
     while (_is_stop.load(std::memory_order::memory_order_acquire)) {
       task t;
@@ -83,8 +86,11 @@ class task_queue {
 
       run_one_task();
     }
+
+    thread_num.fetch_sub(1, std::memory_order::memory_order_release);
   }
 
+  // max thread
   void extra_run() {
     while (_is_stop.load(std::memory_order::memory_order_acquire)) {
       if (_task_num < _core_task_num) {
@@ -93,9 +99,15 @@ class task_queue {
 
       run_one_task();
     }
+
+    if (_task_num > 0) {
+      run_one_task();
+    }
+
+    thread_num.fetch_sub(1, std::memory_order::memory_order_release);
   }
 
-  bool push_common_pre() {
+  bool push_common_before() {
     if (_is_shundown.load(std::memory_order::memory_order_acquire)) {
       return false;
     }
@@ -107,40 +119,65 @@ class task_queue {
     if (_task_num >= _max_task_num) {
       return false;
     }
+    return true;
+  }
+
+  void push_common_after() {
+    _idle.notify_one();
+
+    uint16_t thread_num =
+        _thread_num.load(std::memory_order::memory_order_acquire);
+
+    if (thread_num < _core_thread_num) {
+      const std::unique_lock<LockType> lock(_lock);
+      thread_num = _thread_num.load(std::memory_order::memory_order_relaxed);
+
+      if (thread_num < _core_thread_num && thread_num < _task_num) {
+        std::thread t(&task_queue::core_run, this);
+        _threads.push(t);
+        thread_num.fetch_add(1, std::memory_order::memory_order_relaxed);
+      }
+    }
+
+    thread_num = _thread_num.load(std::memory_order::memory_order_acquire);
+    if (thread_num < _max_thread_num) {
+      const std::unique_lock<LockType> lock(_lock);
+
+      thread_num = _thread_num.load(std::memory_order::memory_order_relaxed);
+      if (thread_num < _max_thread_num && thread_num < _task_num) {
+        std::thread t(&task_queue::extra_run, this);
+        t.detach();
+        thread_num.fetch_add(1, std::memory_order::memory_order_relaxed);
+      }
+    }
+  }
+
+  template <typename T, typename... Args>
+  bool push_common(T* t, Args... args) {
+    if (!push_common_before()) {
+      return false;
+    }
 
     {
       const std::unique_lock<LockType> lock(_lock);
       if (_task_num >= _max_task_num) {
         return false;
       }
-      if (++_task_num == 1) {
-        lock.unlock();
-        _idle.notify_one();
-      }
+      ++_task_num;
+      t->push(args...);
     }
 
-    return true;
-  }
-
-  bool push_common(const task& t, queue<task>* q) {
-    if (!push_common_pre()) {
-      return false;
-    }
-    q->push(t);
-    return true;
+    push_common_after();
+    retrun true;
   }
 
  public:
-  bool push(const task& t) { return push_common(t, _normal_tasks); }
+  bool push_normal(const task& t) { return push_common(&_normal_tasks, t); }
 
-  bool push_urgent(const task& t) { return push_common(t, _urgent_tasks); }
+  bool push_urgent(const task& t) { return push_common(&_urgent_tasks, t); }
 
-  bool push(const uint8_t priority, const task& t) {
-    if (!push_common_pre()) {
-      return false;
-    }
-    _priority_tasks.push(priority, t);
-    return true;
+  bool push_priority(const uint8_t priority, const task& t) {
+    return push_common(&_priority_tasks, priority, t);
   }
 
   void shundown() {
@@ -157,14 +194,14 @@ class task_queue {
   uint32_t _task_num;
   std::atomic_bool _is_stop;
   std::atomic_bool _is_shundown;
+  std::atomic_uint16_t _thread_num;
   LockType _lock;
   std::condition_variable_any _idle;
   queue<task> _urgent_tasks;
   heap<uint8_t, task> _priority_tasks;
   queue<task> _normal_tasks;
   list<std::thread> _threads;
-}  // namespace sun::sync
-__attribute__((__packed__));  // class task_queue
+} __attribute__((__packed__));  // class task_queue
 
 }  // namespace sun::sync
 
