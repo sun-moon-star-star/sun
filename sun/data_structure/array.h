a05a5856b12faa9102d5d6d575c29beedb8156f3
sun-moon-star-star <75414051+sun-moon-star-star@users.noreply.github.com>
2021-01-22 10:14:22 +0800

    fix(data_structure/array): memory leak

--- sun/data_structure/array.h
+++ sun/data_structure/array.h
@@ -16,13 +16,11 @@ namespace sun::data_structure {
 
 template <typename T>
 struct array final {
-  const uint64_t capcity;
-
   array(const uint64_t capcity = UINT64_MAX)
-      : capcity(capcity), _data(nullptr), _alloc_size(0), _use_size(0) {}
+      : _capcity(capcity), _data(nullptr), _alloc_size(0), _use_size(0) {}
 
   array(const array& other)
-      : capcity(other.capcity),
+      : _capcity(other._capcity),
         _data(nullptr),
         _alloc_size(other._alloc_size),
         _use_size(other._use_size) {
@@ -41,7 +39,8 @@ struct array final {
       free(_data);
       _data = nullptr;
     }
-    *const_cast<uint64_t*>(&capcity) = other.capcity;
+
+    _capcity = other._capcity;
     _alloc_size = other._alloc_size;
     _use_size = other._use_size;
     _data = reinterpret_cast<T*>(malloc(sizeof(T) * _alloc_size));
@@ -56,38 +55,36 @@ struct array final {
       while (_use_size) {
         _data[--_use_size].~T();
       }
-      _alloc_size = 0;
 
       free(_data);
+      _alloc_size = 0;
       _data = nullptr;
     }
   }
 
   void swap(array* other) {
-    const uint64_t tmp = other->capcity;
-    *const_cast<uint64_t*>(&(other->capcity)) = capcity;
-    *const_cast<uint64_t*>(&capcity) = tmp;
-
+    std::swap(other->_capcity, this->_capcity);
     std::swap(other->_data, this->_data);
     std::swap(other->_use_size, this->_use_size);
     std::swap(other->_alloc_size, this->_alloc_size);
   }
+
+  uint64_t capcity() const { return _capcity; }
 
   uint64_t size() const { return _use_size; }
 
   uint64_t alloc_size() const { return _alloc_size; }
 
   int fill_n(uint64_t n) {
-    if (unlikely(n + _use_size > capcity)) {
-      return -1;
-    } else if (unlikely(n > UINT64_MAX - _use_size)) {
+    if (unlikely(n > _capcity - _use_size)) {
       return -1;
     }
 
     uint64_t last_use_size = _use_size;
     uint64_t last_alloc_size = _alloc_size;
-    _alloc_size = min(max(last_alloc_size * 3 / 2, last_use_size + n), capcity);
-    _data = reinterpret_cast<T*>(malloc(sizeof(T) * _alloc_size));
+    _alloc_size =
+        min(max(last_alloc_size * 3 / 2, last_use_size + n), _capcity);
+    _data = reinterpret_cast<T*>(realloc(_data, sizeof(T) * _alloc_size));
     _use_size += n;
 
     for (uint64_t i = 0; i < n; ++i) {
@@ -101,15 +98,15 @@ struct array final {
   template <typename P>
   int push_back_common(P&& t) {
     if (_data == nullptr) {
-      _alloc_size = min(8, capcity);
+      _alloc_size = min(8, _capcity);
       _data = reinterpret_cast<T*>(malloc(sizeof(T) * _alloc_size));
     }
 
     if (_use_size < _alloc_size) {
       new (_data + (_use_size++)) T(std::forward<P>(t));
       return 0;
-    } else if (_alloc_size < capcity) {  // _use_size == _alloc_size
-      _alloc_size = min(_alloc_size * 3 / 2, capcity);
+    } else if (_alloc_size < _capcity) {  // _use_size == _alloc_size
+      _alloc_size = min(_alloc_size * 3 / 2, _capcity);
       _data = reinterpret_cast<T*>(realloc(_data, sizeof(T) * _alloc_size));
       new (_data + (_use_size++)) T(std::forward<P>(t));
       return 0;
@@ -144,6 +141,7 @@ struct array final {
   }
 
  private:
+  uint64_t _capcity;
   T* _data;
   uint64_t _alloc_size;
   uint64_t _use_size;
