926519315ab7c792aa3b74d861d12c6fa93aba2e
sun-moon-star-star <75414051+sun-moon-star-star@users.noreply.github.com>
2021-02-03 12:42:42 +0800

    feat(text/statistics): init

--- sun/text/statistics.h
+++ sun/text/statistics.h
@@ -7,81 +7,75 @@
 #define SUN_TEXT_STATISTICS_H_
 
 #include <string>
-#include <unordered_map>
-#include <unordered_set>
 
-namespace sun {
+namespace sun::text {
 
-namespace text {
+class filter;
+
+using filter_ptr = std::shared_ptr<filter>;
 
 class filter {
  public:
-  filter(const std::string& content) : _content(content), _nxt(nullptr) {}
-
-  filter& link(const std::string& content) {
-    if (_nxt) {
-      delete _nxt;
-    }
-    _nxt = new filter(content);
-    return *_nxt;
+  static filter_ptr make_filter(const std::string& content) {
+    filter_ptr obj(new filter(content));
+    return obj;
   }
 
-  bool operator()(const std::string& text) const {
-    filter* cur = const_cast<filter*>(this);
-    while (cur) {
-      if (!cur->judge(text)) {
-        return false;
-      }
-      cur = cur->_nxt;
-    }
-    return false;
+  static filter_ptr make_filter(std::string&& content) {
+    filter_ptr obj(new filter(std::move(content)));
+    return obj;
+  }
+
+  static filter_ptr make_filter(const std::string& content, filter_ptr nxt) {
+    filter_ptr obj(new filter(content, nxt));
+    return obj;
+  }
+
+  static filter_ptr make_filter(std::string&& content, filter_ptr nxt) {
+    filter_ptr obj(new filter(std::move(content), nxt));
+    return obj;
   }
 
  private:
-  inline bool judge(const std::string& text) const {
-    return text.find(_content) == std::string::npos;
+  explicit filter(const std::string& content)
+      : _content(content), _nxt(nullptr) {}
+
+  explicit filter(std::string&& content)
+      : _content(std::move(content)), _nxt(nullptr) {}
+
+  filter(const std::string& content, filter_ptr nxt)
+      : _content(content), _nxt(nxt) {}
+
+  filter(std::string&& content, filter_ptr nxt)
+      : _content(std::move(content)), _nxt(nxt) {}
+
+ public:
+  filter_ptr static link(const std::string& content, filter_ptr other) {
+    filter_ptr obj(new filter(content, other));
+    return obj;
+  }
+
+  bool match(const std::string& text) const {
+    if (!text_match(text)) {
+      return false;
+    }
+
+    if (_nxt == nullptr) {
+      return true;
+    }
+    return _nxt->match(text);
+  }
+
+ private:
+  inline bool text_match(const std::string& text) const {
+    return text.find(_content) != std::string::npos;
   }
 
  public:
   const std::string _content;
-  filter* _nxt;
+  filter_ptr _nxt;
 };  //
 
-class saver {
- public:
-  saver(const std::string& regex) : _regex(regex), _nxt(nullptr) {}
-
-  saver& link(const std::string& regex) {
-    if (_nxt) {
-      delete _nxt;
-    }
-    _nxt = new saver(regex);
-    return *_nxt;
-  }
-
-  bool operator()(const std::string& text) const {
-    saver* cur = const_cast<saver*>(this);
-    while (cur) {
-      if (!cur->judge(text)) {
-        return false;
-      }
-      cur = cur->_nxt;
-    }
-    return false;
-  }
-
- private:
-  inline bool judge(const std::string& text) const {
-    return text.find(_regex) == std::string::npos;
-  }
-
- public:
-  const std::string _regex;
-  saver* _nxt;
-};  // class saver
-
-}  // namespace text
-
-}  // namespace sun
+}  // namespace sun::text
 
 #endif  //  SUN_TEXT_STATISTICS_H_
