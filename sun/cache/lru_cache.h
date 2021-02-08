b6185ff0f4d4ba873c91456718c884445b8a3dd6
sun-moon-star-star <75414051+sun-moon-star-star@users.noreply.github.com>
2021-02-08 17:36:12 +0800

    feat(cache/lru_cache): expire_func

--- sun/cache/lru_cache.h
+++ sun/cache/lru_cache.h
@@ -6,21 +6,21 @@
 #ifndef SUN_CACHE_LRU_CACHE_H_
 #define SUN_CACHE_LRU_CACHE_H_
 
+#include <functional>
+
 namespace sun::cache {
-
-namespace __lru_cache {
-
-struct lru_node {
-  uint64_t expire_time;
-};
-
-}  // namespace __lru_cache
 
 template <typename T>
 class lru_cache {
  private:
+  using lru_node = struct {
+    uint64_t expire_time;     // 可以是毫秒
+    std::shared_ptr<T> data;  // 零拷贝
+  };
+
+  using expire_func = std::function<void(T*)>;
 };  // class lru_cache
 
-};  // namespace sun::cache
+}  // namespace sun::cache
 
 #endif  // SUN_CACHE_LRU_CACHE_H_
---
+++ sun/net/event_loop.h
@@ -0,0 +1,12 @@
+/**
+ * @file event_loop.h
+ * @author sun-moon-star-star
+ * @ref https://github.com/redis/redis/blob/unstable/src/ae.h
+ */
+
+#ifndef SUN_NET_EVENT_LOOP_H_
+#define SUN_NET_EVENT_LOOP_H_
+
+namespace sun::net {}  // namespace sun::net
+
+#endif  // SUN_NET_EVENT_LOOP_H_
