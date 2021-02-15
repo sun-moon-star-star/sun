15b524ccf9a88a92e3ee282b8753242a6b7c7cc5
sun-moon-star-star <75414051+sun-moon-star-star@users.noreply.github.com>
2021-02-15 23:24:31 +0800

    feat(net/event_loop): init

--- sun/net/event_loop.h
+++ sun/net/event_loop.h
@@ -7,6 +7,23 @@
 #ifndef SUN_NET_EVENT_LOOP_H_
 #define SUN_NET_EVENT_LOOP_H_
 
-namespace sun::net {}  // namespace sun::net
+#include <atomic>
+
+namespace sun::net {
+
+struct event;
+
+class channel;
+
+// todo(rpc)
+struct event_loop {
+  std::atomic_bool stop;
+  int max_size;
+  void* private_data;
+  event* events;
+  channel active_channels;
+};
+
+}  // namespace sun::net
 
 #endif  // SUN_NET_EVENT_LOOP_H_
